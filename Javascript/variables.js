a53df1d93eafcaecbcda2c8bd7644984d64d2b60
Saket Khopkar <saketkhopkar910@gmail.com>
2022-11-20 17:00:51 +0530

    Demonstrating variables in Javascript

---
+++ Javascript/variables.js
@@ -0,0 +1,41 @@
+// Variables
+// Used to store data
+
+// Objects
+// used to stode data as well as function
+
+//using var (global)
+var a=10;
+
+console.log(a);
+a=a+1;
+console.log(a);
+ 
+//using let (block)
+let x=50;
+console.log(x);
+x=x+10
+console.log(x);
+
+//using const (can't change the value)
+const pi=3.14;
+console.log(pi);
+// pi=pi+5;
+// console.log(pi);
+
+let p=105;
+console.log(typeof p);
+let s="Hello"
+console.log(typeof s);
+
+let isValid= true
+console.log(typeof isValid);
+
+let f=3.14;
+console.log(typeof f);
+
+
+
+
+let Student={rollno:104, name:"John Doe", marks:73};
+console.log(typeof Student);
