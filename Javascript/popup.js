f14297b4dae69d141b5f5cc70c1824a177b88233
Saket Khopkar <saketkhopkar910@gmail.com>
2022-11-20 17:04:10 +0530

    Shows the working of popups.

---
+++ Javascript/popup.js
@@ -0,0 +1,23 @@
+//alert
+// alert("Welcome to js")
+
+//confirm
+// function demo()
+// {
+//     var choice=confirm("Are u sure you want to login");
+//     if(choice==true)
+//     {
+//         alert("Welcome");
+
+//     }
+//     else
+//         alert("Thank you");
+// }
+
+//prompt
+
+var age=prompt("Enter age");
+console.log(age);
+console.log(typeof age);
+age= Number(age);
+console.log(typeof age);
