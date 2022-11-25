e6355c0a9ff86aa8ed9f765b2c823f4cef09eda9
Saket Khopkar <saketkhopkar910@gmail.com>
2022-11-25 13:11:48 +0530

    Create addnumbers.js

---
+++ Javascript/addnumbers.js
@@ -0,0 +1,6 @@
+function add(){
+  let x = Number(document.getElementById("fnumber").value);
+  let y = Number(document.getElementById("snumber").value);
+  let sum = x + y;
+  document.getElementById("result").innerHTML="Addtion result is : "+sum;
+  }
