80ca65eae2eb8da800fe74e02bc6ed0a01b4c3ee
Saket Khopkar <saketkhopkar910@gmail.com>
2023-01-07 22:57:10 +0530

    Create 5_inheritance.js

---
+++ TypeScript/dest/5_inheritance.js
@@ -0,0 +1,30 @@
+"use strict";
+class PersonClass {
+    constructor(name, age, address) {
+        this.canSpeak = true;
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+    display() {
+        console.log("Details of Person Object");
+        // Printing using Template Literal
+        console.log(`Name: ${this.name} | Age: ${this.age} | Address: ${this.address}`);
+    }
+}
+class Employee extends PersonClass {
+    constructor(id, salary, name, age, address) {
+        super(name, age, address);
+        this.id = id;
+        this.salary = salary;
+    }
+    print() {
+        // Printing without template literal
+        this.display();
+        console.log("ID: " + this.id);
+        console.log("Salary: " + this.salary);
+    }
+}
+let e1 = new Employee(101, 35000, "E1", 29, "Pune");
+// e1.display()
+e1.print();
