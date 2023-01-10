caf3dfa2f90d3ffadfbea6da1d409927b7b79cb6
Saket Khopkar <saketkhopkar910@gmail.com>
2023-01-10 21:50:20 +0530

    Create 10_module.js

---
+++ TypeScript/dest/10_module.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const _8_interface_1 = require("./8_interface");
+let c1 = new _8_interface_1.Circle("Yellow", 11);
+let circle_result2 = c1.area();
+console.log(`Area of circle: ${circle_result2}`);
