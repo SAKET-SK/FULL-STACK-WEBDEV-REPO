000c5da4234a30cd56f84f8fcf04ba3ae686a89e
Saket Khopkar <saketkhopkar478@gmail.com>
2023-01-31 19:01:06 +0530

    TDF in depth

--- Angular/my-app/src/app/components/tdf/tdf.component.ts
+++ Angular/my-app/src/app/components/tdf/tdf.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-tdf',
   templateUrl: './tdf.component.html',
   styleUrls: ['./tdf.component.css']
 })
-export class TdfComponent {
+export class TdfComponent implements OnInit {
+  
+  courses: string[] = ['C/C++','Java','HTML/CSS/JS','Python','AI/ML']
 
+  constructor() {}
+  ngOnInit(): void {}
+
+  getData(val:any){
+    console.log(val)
+  }
 }
