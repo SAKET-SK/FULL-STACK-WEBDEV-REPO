21c67995d27c09627c25853ccd2f070fcb31f042
Saket Khopkar <saketkhopkar478@gmail.com>
2023-02-01 19:40:00 +0530

    Added necessary files for Reactive Forms

--- Angular/my-app/src/app/app.module.ts
+++ Angular/my-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -36,7 +37,8 @@ import { AreaOfCirclePipe } from './shared/area-of-circle.pipe';
   imports: [
     FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   
   ],
   providers: [],
--- Angular/my-app/src/app/components/rf/rf.component.ts
+++ Angular/my-app/src/app/components/rf/rf.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-rf',
   templateUrl: './rf.component.html',
   styleUrls: ['./rf.component.css']
 })
-export class RfComponent {
+export class RfComponent implements OnInit {
+  
+  empData: any
 
+  courses: string[] = ['C/C++','Java','HTML/CSS/JS','Python','AI/ML']
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.empData = new FormGroup({
+      fname: new FormControl('',[Validators.required, Validators.pattern('[a-zA-Z ]{3,30}')]),
+      lname: new FormControl('',[Validators.required, Validators.pattern('[a-zA-Z ]{3,30}')]),
+      email: new FormControl('',[Validators.required, Validators.pattern('[a-z0-9]+@[a-z]+\.[a-z]{2,3}')]),
+      password: new FormControl('',[Validators.required, Validators.pattern('[a-zA-Z0-9 ]{7,20}')]),
+      phone: new FormControl('',[Validators.required, Validators.pattern('[0-9]{10}')]),
+      courses: new FormControl('',[Validators.required]),
+      terms: new FormControl('',[Validators.requiredTrue]) // This is required for radio button and checkbox
+    })
+  }
+  
+  getData(val:any){
+    console.log(val)
+  }
 }
