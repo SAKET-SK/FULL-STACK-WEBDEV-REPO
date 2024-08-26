9f3169277d54235dc4c5773a40e84f907649a4ff
Saket Khopkar <saketkhopkar910@gmail.com>
2024-08-26 16:45:12 +0530

    Diving deep into React Hooks

---
+++ React/my-first-app/src/components/TitleUpdater.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+
+function TitleUpdater() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `Count: ${count}`;
+  }, [count]);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}
+
+export default TitleUpdater;
